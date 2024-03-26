0684403193904359b9b00e193efc744e124bf5c3
Erfan Heshmati <heshmati74@gmail.com>
2024-03-26 16:36:40 +0330

    img src changed

--- mern-client/src/dashboard/DashboardLayout.jsx
+++ mern-client/src/dashboard/DashboardLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import SideBar from "./Sidebar";
+// import SideBar from "./Sidebar";
 
 
 const DashboardLayout = () => {
     return (
         <div className="flex gap-4 flex-col md:flex-row">
-            <SideBar />
+            {/* <SideBar /> */}
             <Outlet />
         </div>
     )
