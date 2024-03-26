a1141e72332e85c98a9157156205328e8408b789
Erfan Heshmati <heshmati74@gmail.com>
2024-03-26 16:46:40 +0330

    dashboard layout

--- mern-client/src/dashboard/DashboardLayout.jsx
+++ mern-client/src/dashboard/DashboardLayout.jsx
@@ -1,14 +1,14 @@
-// import { Outlet } from "react-router-dom";
-// import SideBar from "./Sidebar";
+import { Outlet } from "react-router-dom";
+import SideBar from "./Sidebar";
 
 
-// const DashboardLayout = () => {
-//     return (
-//         <div className="flex gap-4 flex-col md:flex-row">
-//             <SideBar />
-//             <Outlet />
-//         </div>
-//     )
-// }
+const DashboardLayout = () => {
+    return (
+        <div className="flex gap-4 flex-col md:flex-row">
+            <SideBar />
+            <Outlet />
+        </div>
+    )
+}
 
-// export default DashboardLayout;
+export default DashboardLayout;
